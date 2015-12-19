a14155361a89124b9eb78f564949fd30e4c9b2f1
Hal Deadman <deadman_harold@bah.com>
2015-12-18 19:32:59 -0500

    revert last two changes

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,13 +6,6 @@ class shinken::service (
     'present' => true,
     'absent'  => false,
   }
-  
-  #exported nagios config resources are read-only by root
-  exec { 'fix-permissions':
-     command     => "find /etc/shinken/hosts -type f -name '*cfg' | xargs chmod go+r",
-     path        => ["/usr/bin"],
-     refreshonly => true,
-  } ->   
   service{ 'shinken':
     ensure     => $service,
     enable     => $service,
